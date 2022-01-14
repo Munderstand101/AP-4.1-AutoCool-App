7649052597b48b37168cd305a0371020fe8678a8
Munderstand <Stefan@DESKTOP-FDFKV7J>
2022-01-14 13:49:27 +0100

    Creation des Activity

---
+++ app/src/main/java/com/munderstand/autocool/ModifierVoitureActivity.java
@@ -0,0 +1,14 @@
+package com.munderstand.autocool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifierVoitureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifier_voiture);
+    }
+}
